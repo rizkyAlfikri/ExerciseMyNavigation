92a0046866dfbbebc2519044b8916a4054d89616
rizkyAlfikri <rizkyalfikri@gmail.com>
2020-01-09 20:51:11 +0700

    set action and put data for btn category lifestyle in category fragment

--- app/src/main/java/com/dicoding/picodiploma/mynavigation/CategoryFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/mynavigation/CategoryFragment.kt
@@ -6,11 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_category.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CategoryFragment : Fragment() {
+
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_STOCK = "extra_stock"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +27,15 @@ class CategoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_category_lifestyle.setOnClickListener {view ->
+            val  bundle = Bundle()
+            bundle.putString(EXTRA_NAME, "Lifestyle")
+            bundle.putLong(EXTRA_STOCK, 7)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment)
+        }
+    }
 
 }
