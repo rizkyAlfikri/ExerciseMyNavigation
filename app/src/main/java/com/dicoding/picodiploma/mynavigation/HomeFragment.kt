16031f3d3cb6a9ff75f2a406dffb28236b4f56c8
rizkyAlfikri <rizkyalfikri@gmail.com>
2020-01-09 20:35:28 +0700

    edit action for btn_category and btn_profile

--- app/src/main/java/com/dicoding/picodiploma/mynavigation/HomeFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/mynavigation/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -23,12 +24,12 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        btn_category.setOnClickListener {
+        btn_category.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
-        }
+        )
 
         btn_profile.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_profileActivity)
+            it.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
         }
     }
 
